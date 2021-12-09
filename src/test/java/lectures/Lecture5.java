cebf9227b8938b6e6d4f09e421b6295c385c93ca
Michael Heinecke <michael.heinecke.1337@gmail.com>
2021-12-09 21:22:31 +0100

    Filter, mapping, calculate average

--- src/test/java/lectures/Lecture5.java
+++ src/test/java/lectures/Lecture5.java
@@ -1,43 +1,62 @@
 package lectures;
 
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import beans.Car;
 import beans.Person;
 import beans.PersonDTO;
 import com.google.common.collect.ImmutableList;
-import java.util.List;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
 import mockdata.MockData;
 import org.junit.Test;
 
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class Lecture5 {
 
-  @Test
-  public void understandingFilter() throws Exception {
-    ImmutableList<Car> cars = MockData.getCars();
+    @Test
+    public void understandingFilter() throws Exception {
+        ImmutableList<Car> cars = MockData.getCars();
 
-  }
+        final Predicate<Car> carPredicate = car -> car.getPrice() < 20000;
 
-  @Test
-  public void ourFirstMapping() throws Exception {
-    // transform from one data type to another
-    List<Person> people = MockData.getPeople();
+        List<Car> carsFiltered = cars.stream()
+                .filter(carPredicate)
+                .collect(Collectors.toList());
 
-  }
+        carsFiltered.forEach(System.out::println);
+    }
 
-  @Test
-  public void averageCarPrice() throws Exception {
-    // calculate average of car prices
+    @Test
+    public void ourFirstMapping() throws Exception {
+        // transform from one data type to another
+        List<Person> people = MockData.getPeople();
+        List<PersonDTO> dtos = people.stream()
+                .map(person -> new PersonDTO(person.getId(), person.getFirstName(), person.getAge()))
+                .collect(Collectors.toList());
 
-  }
+        dtos.forEach(System.out::println);
+        assertThat(dtos).hasSize(1000);
+    }
 
-  @Test
-  public void test() throws Exception {
+    @Test
+    public void averageCarPrice() throws Exception {
+        // calculate average of car prices
+        List<Car> cars = MockData.getCars();
+        double averageCarPrice = cars.stream()
+                .mapToDouble(Car::getPrice)
+                .average()
+                .orElse(0);
 
-  }
+        System.out.println(averageCarPrice);
+    }
+
+    @Test
+    public void test() throws Exception {
+        MockData.getCars().forEach(System.out::println);
+    }
 }
 
 
