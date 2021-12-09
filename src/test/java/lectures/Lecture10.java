9f6ffa81660dcf79e2abddece2d24791af0c1f0e
Michael Heinecke <michael.heinecke.1337@gmail.com>
2021-12-09 22:37:18 +0100

    flatMap

--- src/test/java/lectures/Lecture10.java
+++ src/test/java/lectures/Lecture10.java
@@ -2,36 +2,48 @@ package lectures;
 
 
 import com.google.common.collect.Lists;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Lecture10 {
 
-  private static final List<ArrayList<String>> arrayListOfNames = Lists.newArrayList(
-      Lists.newArrayList("Mariam", "Alex", "Ismail"),
-      Lists.newArrayList("John", "Alesha", "Andre"),
-      Lists.newArrayList("Susy", "Ali")
-  );
+    private static final List<ArrayList<String>> arrayListOfNames = Lists.newArrayList(
+            Lists.newArrayList("Mariam", "Alex", "Ismail"),
+            Lists.newArrayList("John", "Alesha", "Andre"),
+            Lists.newArrayList("Susy", "Ali")
+    );
 
-  @Before
-  public void setUp() {
-    System.out.println(arrayListOfNames);
-  }
+    @Before
+    public void setUp() {
+        System.out.println(arrayListOfNames);
+    }
 
-  @Test
-  public void withoutFlatMap() throws Exception {
+    @Test
+    public void withoutFlatMap() throws Exception {
 //    [Mariam, Alex, Ismail, John, Alesha, Andre, Susy, Ali]
+        List<String> flatList = new ArrayList<>();
+        IntStream.range(0, arrayListOfNames.size()).forEach(i ->
+                IntStream.range(0, arrayListOfNames.get(i).size()).forEach(j ->
+                        flatList.add(arrayListOfNames.get(i).get(j)))
+        );
 
-  }
+        System.out.println(flatList);
+    }
 
-  @Test
-  public void withFlatMap() throws Exception {
+    @Test
+    public void withFlatMap() throws Exception {
 //   [Mariam, Alex, Ismail, John, Alesha, Andre, Susy, Ali]
+        List<String> flatList = arrayListOfNames.stream()
+                .flatMap(List::stream)
+                .collect(Collectors.toList());
 
-  }
+        System.out.println(flatList);
+    }
 
 }
 
