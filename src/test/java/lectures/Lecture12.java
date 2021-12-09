1922427d510d8cd8d829e798470a75717a6b6e0b
Michael Heinecke <michael.heinecke.1337@gmail.com>
2021-12-09 22:59:57 +0100

    Understanding collect - collect can be used for custom data structures

--- src/test/java/lectures/Lecture12.java
+++ src/test/java/lectures/Lecture12.java
@@ -1,20 +1,33 @@
 package lectures;
 
 import beans.Person;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 import mockdata.MockData;
 import org.junit.Test;
 
-public class Lecture12 {
-  @Test
-  public void understandingCollect() throws Exception {
-    List<String> emails = MockData.getPeople()
-        .stream()
-        .map(Person::getEmail)
-        .collect(Collectors.toList());
+import java.util.ArrayList;
+import java.util.List;
 
-    emails.forEach(System.out::println);
-  }
+public class Lecture12 {
+    @Test
+    public void understandingCollect() throws Exception {
+        List<String> emails = MockData.getPeople()
+                .stream()
+                .map(Person::getEmail)
+                .collect(
+                        ArrayList::new,
+                        List::add,
+                        List::addAll
+                );
+
+        List<String> emails2 = MockData.getPeople()
+                .stream()
+                .map(Person::getEmail)
+                .collect(
+                        () -> new ArrayList<>(),
+                        (list, element) -> list.add(element),
+                        (list1, list2) -> list1.addAll(list2)
+                );
+
+        emails.forEach(System.out::println);
+    }
 }
