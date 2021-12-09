48530d1935dcd289f8f9bf01cd1b78723ea00bf2
Michael Heinecke <michael.heinecke.1337@gmail.com>
2021-12-09 18:16:55 +0100

    Find min and max in list

--- src/test/java/lectures/Lecture3.java
+++ src/test/java/lectures/Lecture3.java
@@ -1,23 +1,26 @@
 package lectures;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import com.google.common.collect.ImmutableList;
+import org.junit.Test;
+
 import java.util.Comparator;
 import java.util.List;
-import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class Lecture3 {
 
-  @Test
-  public void min() throws Exception {
-    final List<Integer> numbers = ImmutableList.of(1, 2, 3, 100, 23, 93, 99);
+    @Test
+    public void min() {
+        final List<Integer> numbers = ImmutableList.of(1, 2, 3, 100, 23, 93, 99);
+        int min = numbers.stream().min(Comparator.naturalOrder()).get();
+        assertThat(min).isEqualTo(1);
+    }
 
-  }
-
-  @Test
-  public void max() throws Exception {
-    final List<Integer> numbers = ImmutableList.of(1, 2, 3, 100, 23, 93, 99);
-
-  }
+    @Test
+    public void max() {
+        final List<Integer> numbers = ImmutableList.of(1, 2, 3, 100, 23, 93, 99);
+        int max = numbers.stream().max(Comparator.naturalOrder()).get();
+        assertThat(max).isEqualTo(100);
+    }
 }
