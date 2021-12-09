8fe59b6d4f454ac57cfacf39df25f6d7fd9eb1bf
Michael Heinecke <michael.heinecke.1337@gmail.com>
2021-12-09 22:47:58 +0100

    Joining strings

--- src/test/java/lectures/Lecture11.java
+++ src/test/java/lectures/Lecture11.java
@@ -1,22 +1,30 @@
 package lectures;
 
 import com.google.common.collect.ImmutableList;
+import org.junit.Test;
+
 import java.util.List;
 import java.util.stream.Collectors;
-import mockdata.MockData;
-import org.junit.Test;
 
 public class Lecture11 {
 
-  @Test
-  public void joiningStrings() throws Exception {
-    List<String> names = ImmutableList.of("anna", "john", "marcos", "helena", "yasmin");
+    @Test
+    public void joiningStrings() throws Exception {
+        List<String> names = ImmutableList.of("anna", "john", "marcos", "helena", "yasmin");
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < names.size(); i++) {
+            sb.append(names.get(i));
+            if (i != names.size() - 1) {
+                sb.append(", ");
+            }
+        }
+        System.out.println(sb);
+    }
 
-  }
-
-  @Test
-  public void joiningStringsWithStream() throws Exception {
-    List<String> names = ImmutableList.of("anna", "john", "marcos", "helena", "yasmin");
-
-  }
+    @Test
+    public void joiningStringsWithStream() throws Exception {
+        List<String> names = ImmutableList.of("anna", "john", "marcos", "helena", "yasmin");
+        String concat = names.stream().collect(Collectors.joining(", "));
+        System.out.println(concat);
+    }
 }
