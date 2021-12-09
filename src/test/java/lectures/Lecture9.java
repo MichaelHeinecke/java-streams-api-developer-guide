31eec58cd592fa143144bdd2d74d199acb050efc
Michael Heinecke <michael.heinecke.1337@gmail.com>
2021-12-09 22:37:00 +0100

    Reduce

--- src/test/java/lectures/Lecture9.java
+++ src/test/java/lectures/Lecture9.java
@@ -1,18 +1,21 @@
 package lectures;
 
 
-import com.google.common.collect.Lists;
-import java.util.Arrays;
 import org.junit.Test;
+
+import java.util.Arrays;
 
 public class Lecture9 {
 
-  @Test
-  public void reduce() throws Exception {
-    Integer[] integers = {1, 2, 3, 4, 99, 100, 121, 1302, 199};
+    @Test
+    public void reduce() throws Exception {
+        Integer[] integers = {1, 2, 3, 4, 99, 100, 121, 1302, 199};
+        int sum = Arrays.stream(integers).reduce(Integer::sum).get();
+        System.out.println(sum);
 
-  }
-
+        int sum2 = Arrays.stream(integers).reduce(0, (x, y) -> x + y);
+        System.out.println(sum2);
+    }
 
 }
 
